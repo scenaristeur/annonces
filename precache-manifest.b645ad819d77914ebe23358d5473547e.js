9412f412b6d44620c5eb9dc5dddf65b20d2bda70
Scenaristeur <scenrister@galcm>
2021-03-23 20:28:53 +0100

    send notif to agora

---
+++ precache-manifest.b645ad819d77914ebe23358d5473547e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2471c0ed2ab683bd9fe1f97d4237e4",
+    "url": "/bon-prix/404.html"
+  },
+  {
+    "revision": "3f76fd6e4cf40afd6173",
+    "url": "/bon-prix/css/app.be22d1ed.css"
+  },
+  {
+    "revision": "b58c3f370710db253a47",
+    "url": "/bon-prix/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "0c5636c558ac8f500c5b95ead63a6300",
+    "url": "/bon-prix/index.html"
+  },
+  {
+    "revision": "1444629ed36537011978",
+    "url": "/bon-prix/js/about.63775c52.js"
+  },
+  {
+    "revision": "3033ba7e922e3b0ed686",
+    "url": "/bon-prix/js/annonceEdit.6dfb50ff.js"
+  },
+  {
+    "revision": "6447165c52ecebcd18cb",
+    "url": "/bon-prix/js/annonceList.7d66848d.js"
+  },
+  {
+    "revision": "3f76fd6e4cf40afd6173",
+    "url": "/bon-prix/js/app.8726bbe7.js"
+  },
+  {
+    "revision": "41567bdb958078142d86",
+    "url": "/bon-prix/js/chunk-2d0ba146.a6650fc5.js"
+  },
+  {
+    "revision": "25adca8e8ca887863802",
+    "url": "/bon-prix/js/chunk-2d226c5a.29858aa2.js"
+  },
+  {
+    "revision": "2fc31f176add1d2e7034",
+    "url": "/bon-prix/js/chunk-2d2315f6.f1471172.js"
+  },
+  {
+    "revision": "777e0eea4643a66b67ab",
+    "url": "/bon-prix/js/chunk-666663cd.fcbb8cc7.js"
+  },
+  {
+    "revision": "725f9f88a8a0fa6430e4",
+    "url": "/bon-prix/js/chunk-7e121fa4.8d40d8ce.js"
+  },
+  {
+    "revision": "b58c3f370710db253a47",
+    "url": "/bon-prix/js/chunk-vendors.311c68c4.js"
+  },
+  {
+    "revision": "94e919648d74d1169167d029c517eb07",
+    "url": "/bon-prix/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/bon-prix/robots.txt"
+  }
+]);
