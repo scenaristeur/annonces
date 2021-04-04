195ba19d173a6d3b6c59ab0733c7c464063dd326
Scenaristeur <scenrister@galcm>
2021-04-04 02:27:00 +0200

    Edit start dropzone for resize on upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bon-prix/precache-manifest.4c65d638b8844d9c9aa04b6a4dadd5b4.js"
+  "/bon-prix/precache-manifest.bab7f173b8f66fc5232d59801362ec41.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "bon-prix"});
