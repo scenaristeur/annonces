04ef9c27e619bbb6348753418daa5d9859fdf795
Scenaristeur <scenrister@galcm>
2021-04-04 20:33:56 +0200

    parse inbox acl

--- src/components/annonce/AnnonceInbox.vue
+++ src/components/annonce/AnnonceInbox.vue
@@ -2,29 +2,8 @@
 
   <div>
     <div v-if="webId != null">
-      <div v-if="acl.status != 'OK'">
-        <h5>ACL / Authorization verification : </h5>
-        <p>
-          To allow other Authenticated Users to send you inbox message, you have to set correct Authorizations for <b-badge variant="warning">{{ inboxAnnonceFolder }}</b-badge> as i'm not sure the are good :
-          <ul>
-            <li>
-              <b-badge variant="warning">{{webId}}</b-badge> with <b-badge variant="warning">CONTROL</b-badge>
-            </li>
-            <li>
-              AuthenticatedAgent as "Poster" for the folder & "Submitters" for it's content
-            </li>
-          </ul>
-          <br>
 
-          <a href="https://github.com/jeff-zucker/solid-file-client/issues/189" target="_blank">
-            More info about why You must change authorization on this folder</a>
-          </p>
-          You can do it manually at <a :href="inboxAnnonceFolder" target="_blank">{{ inboxAnnonceFolder }}</a> or use this button. <br>
-          <b-button @click="setAcl" v-if="acl.status == 'KO'">Set Authorization</b-button>
-          <!-- <small>
-          {{ acl }}
-        </small> -->
-      </div>
+      <AclCheck :path="inboxAnnonceFolder" :authorizations="authorizations" :webId="webId" />
 
 
       <b-list-group>
@@ -62,16 +41,21 @@ import auth from 'solid-auth-client';
 import FC from 'solid-file-client'
 const fc = new FC( auth )
 let ldflex = window.solid
+
 // import { v4 as uuidv4 } from 'uuid';
 
 export default {
   name: "AnnonceInbox",
+  components: {
+    'AclCheck': () => import('@/components/solid/AclCheck'),
+  },
   data() {
     return {
       messages: [],
       deleteMessage: "",
       inboxAnnonceFolder: "",
-      acl: {status: "checking"}
+      authorizations: [],
+      //  acl: {status: "checking"}
     }
   },
   async created(){
@@ -79,160 +63,45 @@ export default {
   },
   methods: {
     async init(){
+
       if (this.webId != null){
         //get inbox Folder
-        this.acl.status = "finding webId"
         let inboxFolder = await ldflex.data.from(this.webId)[this.webId]['http://www.w3.org/ns/ldp#inbox']
-        this.acl.status = "finding inbox"
         // get aannonce inbox folder
         this.inboxAnnonceFolder = await `${inboxFolder}`+"annonces/"
-        this.acl.status = "imagining a annonce folder"
-        console.log(this.webId, this.inboxAnnonceFolder)
-        await this.checkAcl("first run")
-        this.readInbox()
+        //  this.acl.status = "imagining a annonce folder"
+        //  console.log(this.webId, this.inboxAnnonceFolder)
+        // await this.checkAcl("first run")
+        if( !(await fc.itemExists(this.inboxAnnonceFolder)) ) {
+          await fc.createFolder(this.inboxAnnonceFolder) // only create if it doesn't already exist
+        }
       }
-    },
-    async checkAcl(step){
-      // create inbox annonce folder if not exist
-      this.acl.status = "checking if folder exist"
-      if( !(await fc.itemExists(this.inboxAnnonceFolder)) ) {
-        this.acl.status = "creating folder"
-        await fc.createFolder(this.inboxAnnonceFolder) // only create if it doesn't already exist
-      }
-      // check aclObject
-      this.acl.status = "cheching authorizations"
-      this.acl.aclObject = await fc.aclUrlParser(this.inboxAnnonceFolder)
-      console.log(step,"aclObject on ",this.inboxAnnonceFolder,JSON.stringify(this.acl.aclObject, undefined,2))
-      // create Content from aclObject
-      this.acl.aclContent = await fc.acl.createContent(this.inboxAnnonceFolder, this.acl.aclObject/*, options*/)
-      console.log(step,"aclcontent on ",this.inboxAnnonceFolder, this.acl.aclContent)
-      console.log(step,"aclon ",this.inboxAnnonceFolder, this.acl)
-      // say they are KO for the moment as i can't verify see issue : https://github.com/jeff-zucker/solid-file-client/issues/189#issuecomment-812887070
-      this.acl.status = "KO"
+      this.readInbox()
     },
 
-    async setAcl(){
-      // according to recent solid-file-client changes : https://github.com/jeff-zucker/solid-file-client/issues/189#issuecomment-808966875
-      try{
-        this.acl.status = "setting authorizations"
-        let   aclUsers1 = await fc.acl.addUserMode({}, [{ agent: this.webId }], ['Read', 'Write', 'Control'], ['accessTo', 'default'])
-        aclUsers1 = await fc.acl.addUserMode(aclUsers1, [{ agentClass: 'AuthenticatedAgent' }], ['Append'], ['default'])
-
-        const aclUsers2 = await fc.acl.addUserMode({}, [{ agentClass: 'AuthenticatedAgent' }], ['Append', 'Read'], ['accessTo'])
-
-        const aclUsers = [aclUsers1, aclUsers2]
-
-        const aclContentNew = await fc.acl.createContent(this.inboxAnnonceFolder, aclUsers)
-        console.log('build an aclContent ' + aclContentNew)
-
-        const { acl: aclUrl } = await fc.getItemLinks(this.inboxAnnonceFolder, { links: 'include_possible'})
-        //    const links = await fc.getItemLinks(inboxAnnonce, { links: 'include_possible'})
-
-        //  console.log(links)
-        let result = await fc.putFile(aclUrl, aclContentNew, 'text/turtle')
-        console.log("result",result)
-        //this.acl.status = "OK"
-        await this.checkAcl("after update")
-      }catch(e){
-        this.acl.status = "ERROR : "+e
-        alert(e)
-
-      }
-
-
-    },
     async readInbox() {
-
-
-      // @prefix : <#>.
-      // @prefix acl: <http://www.w3.org/ns/auth/acl#>.
-      // @prefix foaf: <http://xmlns.com/foaf/0.1/>.
-      // @prefix target: <./>.
-      //
-      // :ReadAppend
-      //     a acl:Authorization;
-      //     acl:accessTo target:;
-      //     acl:agentClass acl:AuthenticatedAgent;
-      //     acl:mode acl:Read, acl:Append.
-      //
-      // :ReadWriteControl
-      //     a acl:Authorization;
-      //     acl:accessTo target:;
-      //     acl:default target:;
-      //     acl:agent </profile/card#me>;
-      //     acl:agent <https://bourgeoa.solidcommunity.net/profile/card#me>;
-      //     acl:mode acl:Read, acl:Write, acl:Control.
-
-      //       @prefix : <#>.
-      // @prefix acl: <http://www.w3.org/ns/auth/acl#>.
-      // @prefix foaf: <http://xmlns.com/foaf/0.1/>.
-      // @prefix target: <./>.
-      //
-      // :ReadAppend
-      //     a acl:Authorization;
-      //     acl:accessTo target:;
-      //     acl:agentClass acl:AuthenticatedAgent;
-      //     acl:mode acl:Read, acl:Append.
-      //
-      // :ReadWriteControl
-      //     a acl:Authorization;
-      //     acl:accessTo target:;
-      //     acl:agent </profile/card#me>;
-      //     acl:mode acl:Read, acl:Write, acl:Control.
-
-
-      // if (this.webId != null){
-      //   let inboxFolder = await ldflex.data.from(this.webId)[this.webId]['http://www.w3.org/ns/ldp#inbox']
-      //   let inboxAnnonce = `${inboxFolder}`+"annonces/"
-      //   if( !(await fc.itemExists(inboxAnnonce)) ) {
-      //     await fc.createFolder(inboxAnnonce) // only create if it doesn't already exist
-      //   }
-      //
-      //   let aclObject = await fc.aclUrlParser(inboxAnnonce)
-      //   console.log("inherit acl",aclObject)
-      //   let aclContent = await fc.acl.createContent(inboxAnnonce, aclObject/*, options*/)
-      //   console.log(aclContent)
-      //
-      //   // add an other rule
-      //   let   aclUsers = await fc.acl.addUserMode({}, [{ agent: this.webId }], ['Read', 'Write', 'Control'], ['accessTo', 'default'])
-      //   aclUsers = await fc.acl.addUserMode(aclUsers, [{ agentClass: 'AuthenticatedAgent' }], ['Append'], ['default'])
-      //   aclUsers = await fc.acl.addUserMode(aclUsers, [{ agentClass: 'Agent' }], ['Append', 'Read'], ['accessTo'])
-      //
-      //   //      aclUsers = await fc.acl.addUserMode(aclUsers, [{ agent: 'https://bourgeoa.solidcommunity.net/profile/card#me' }], ['Read', 'Write', 'Control'], ['accessTo', 'default'])
-      //
-      //
-      //   //  let aclUsers = await fc.acl.addUserMode({}, [{ agentClass: 'AuthenticatedAgent'}], ['Read', 'Write'])
-      //   const aclContentNew = await fc.acl.createContent(inboxAnnonce, aclUsers)
-      //   console.log('build an aclContent ' + aclContentNew)
-      //
-      //   const { acl: aclUrl } = await fc.getItemLinks(inboxAnnonce, { links: 'include_possible'})
-      //   //    const links = await fc.getItemLinks(inboxAnnonce, { links: 'include_possible'})
-      //
-      //   //  console.log(links)
-      //   let result = await fc.putFile(aclUrl, aclContentNew, 'text/turtle')
-      //   console.log("result",result)
-
-
-      let folder = await fc.readFolder(this.inboxAnnonceFolder)
-      console.log(folder)
-      let files = folder.files.sort((a,b) => new Date(b.created).getTime() - new Date(a.created).getTime());
-      // console.log(files)
-      this.messages = []
-      //
-      while ( files.length > 0){
-        let f = files.shift()
-        let m = await JSON.parse(await fc.readFile(f.url))
-        m.url = f.url
-        this.messages.push(m)
-        //  console.log(this.messages)
-        //  console.log(f)
-        // let resource = f.url+"#this"
-        // for await (const annonce_url of ldflex.data[resource]['http://purl.org/dc/terms/hasPart']){
-        //   let idx = this.annonces.findIndex(x => x.url === `${annonce_url}`)
-        //   if (idx === -1) {
-        //     this.annonces.push({url: `${annonce_url}`})
-        //   }
-        // }
+      if (this.inboxAnnonceFolder != undefined && this.inboxAnnonceFolder.length > 0){
+        let folder = await fc.readFolder(this.inboxAnnonceFolder)
+        console.log(folder)
+        let files = folder.files.sort((a,b) => new Date(b.created).getTime() - new Date(a.created).getTime());
+        // console.log(files)
+        this.messages = []
+        //
+        while ( files.length > 0){
+          let f = files.shift()
+          let m = await JSON.parse(await fc.readFile(f.url))
+          m.url = f.url
+          this.messages.push(m)
+          //  console.log(this.messages)
+          //  console.log(f)
+          // let resource = f.url+"#this"
+          // for await (const annonce_url of ldflex.data[resource]['http://purl.org/dc/terms/hasPart']){
+          //   let idx = this.annonces.findIndex(x => x.url === `${annonce_url}`)
+          //   if (idx === -1) {
+          //     this.annonces.push({url: `${annonce_url}`})
+          //   }
+          // }
+        }
       }
       //}
     },
---
+++ src/components/solid/AclCheck.vue
@@ -0,0 +1,256 @@
+<template>
+  <div>
+    <h5>Acl for {{ path }} : </h5>
+    <div v-if="acl.status != 'OK'">
+      <h5>ACL / Authorization verification : </h5>
+      <p>
+        To allow other Authenticated Users to send you inbox message, you have to set correct Authorizations for <b-badge variant="warning">{{ path }}</b-badge> as i'm not sure the are good :
+        <ul>
+          <li>
+            <b-badge variant="warning">{{webId}}</b-badge> with <b-badge variant="warning">CONTROL</b-badge>
+          </li>
+          <li>
+            AuthenticatedAgent as "Poster" for the folder & "Submitters" for it's content
+          </li>
+        </ul>
+        <br>
+
+        {{ acl.aclObject}}
+        <hr>
+        <p>
+
+
+
+          Acl for <b-badge variant="warning">{{path}}</b-badge>
+          <b-list-group>
+            <b-list-group-item v-for="(aclAuth,i) in acl.aclObject" :key="i">
+              <div v-if="aclAuth">
+              aclAuth is ARRAY ?   {{ Array.isArray(aclAuth)}}
+                <div v-for="(value, k) in Object.entries(aclAuth)" :key="k">
+                  <div v-if="value[1].mode">
+                    <h5>{{value[0].split('&')[0]}} : <b-badge variant="warning">{{value[0].split('&')[1]}}</b-badge></h5>
+                    <b>Can :</b>
+                    <b-badge :variant="value[1].mode.Read == 1? 'success': 'danger'">READ</b-badge>
+                    <b-badge :variant="value[1].mode.Append == 1? 'success': 'danger'">APPEND</b-badge>
+                    <b-badge :variant="value[1].mode.Write == 1? 'success': 'danger'">WRITE</b-badge>
+                    <b-badge :variant="value[1].mode.Control == 1? 'success': 'danger'">CONTROL</b-badge>
+
+                    <!-- <b-badge variant="warning">  APPEND : {{value[1].mode.Append}}</b-badge>
+                    <b-badge variant="warning">  WRITE : {{value[1].mode.Write}}</b-badge>
+                    <b-badge variant="warning">  CONTROL : {{value[1].mode.Control}}</b-badge> -->
+                    <!-- <div v-for="(mode, m) in Object.entries(value[1].mode)" :key="m">
+                    <b-badge variant="warning">{{mode}}</b-badge>
+
+                  </div> -->
+
+                  <br>
+                  <b>What ?</b>
+                  <!-- {{value[1].access}} -->
+                  <b-badge v-if="value[1].access.accessTo == 1">{{path}}</b-badge>
+                  <div  v-if="value[1].access.default == 1">content of
+                    <b-badge> {{ path }}</b-badge>
+                  </div>
+
+                  <br>
+                  <!-- <b>And:</b> {{value[1].key}}<br> -->
+                  <br>
+                </div>
+                <div v-else>
+                  {{ value[1]}} should have mode
+                </div>
+              </div>
+            </div>
+          </b-list-group-item>
+        </b-list-group>
+
+
+        <!-- <b-table striped hover responsive  :items="acl.aclObject">
+        <template #cell()="data">
+        <b>Agent:</b> {{data.item}} : {{data.item}}<br>
+        <b>Mode :</b> {{data.item.mode}}<br>
+        <b>Access:</b> {{data.item.access}}
+      </template>
+    </b-table> -->
+
+
+  </p>
+
+
+  <!-- <a href="https://github.com/jeff-zucker/solid-file-client/issues/189" target="_blank">
+  More info about why You must change authorization on this folder</a>
+</p> -->
+You can do it manually at <a :href="path" target="_blank">{{ path }}</a> or use this button. <br>
+<b-button @click="setAcl" v-if="acl.status == 'KO'">Set Authorization</b-button>
+<!-- <small>
+{{ acl }}
+</small> -->
+</div>
+<div v-else>
+  ACL OK
+  {{ acl }}
+
+</div>
+</div>
+</template>
+
+<script>
+import auth from 'solid-auth-client';
+import FC from 'solid-file-client'
+const fc = new FC( auth )
+
+
+export default {
+  name: "AclCheck",
+  props: ['path', 'authorizations', 'webId'],
+  data() {
+    return {
+      acl: {status: "checking"}
+    }
+  },
+  async created(){
+    this.init()
+  },
+  methods: {
+    async init(){
+      if (this.webId != null && this.path.length > 0){
+        this.acl.status = "imagining a annonce folder"
+        console.log(this.webId, this.path)
+
+        if( !(await fc.itemExists(this.path)) ) {
+          this.acl.status = "creating folder"
+          await fc.createFolder(this.path) // only create if it doesn't already exist
+        }
+        await this.checkAcl("first run")
+
+      }
+    },
+    async checkAcl(step){
+      // create inbox annonce folder if not exist
+      this.acl.status = "checking if folder exist"
+
+      // check aclObject
+      this.acl.status = "cheching authorizations"
+      this.acl.aclObject = await fc.aclUrlParser(this.path)
+      console.log(step,"aclObject on ",this.path,JSON.stringify(this.acl.aclObject, undefined,2))
+      // create Content from aclObject
+      this.acl.aclContent = await fc.acl.createContent(this.path, this.acl.aclObject/*, options*/)
+      console.log(step,"aclcontent on ",this.path, this.acl.aclContent)
+      console.log(step,"aclon ",this.path, this.acl)
+      // say they are KO for the moment as i can't verify see issue : https://github.com/jeff-zucker/solid-file-client/issues/189#issuecomment-812887070
+      this.acl.status = "KO"
+    },
+
+    async setAcl(){
+      // according to recent solid-file-client changes : https://github.com/jeff-zucker/solid-file-client/issues/189#issuecomment-808966875
+      try{
+        this.acl.status = "setting authorizations"
+        let   aclUsers1 = await fc.acl.addUserMode({}, [{ agent: this.webId }], ['Read', 'Write', 'Control'], ['accessTo', 'default'])
+        aclUsers1 = await fc.acl.addUserMode(aclUsers1, [{ agentClass: 'AuthenticatedAgent' }], ['Append'], ['default'])
+
+        const aclUsers2 = await fc.acl.addUserMode({}, [{ agentClass: 'AuthenticatedAgent' }], ['Append', 'Read'], ['accessTo'])
+
+        const aclUsers = [aclUsers1, aclUsers2]
+
+        const aclContentNew = await fc.acl.createContent(this.path, aclUsers)
+        console.log('build an aclContent ' + aclContentNew)
+
+        const { acl: aclUrl } = await fc.getItemLinks(this.path, { links: 'include_possible'})
+        //    const links = await fc.getItemLinks(inboxAnnonce, { links: 'include_possible'})
+
+        //  console.log(links)
+        let result = await fc.putFile(aclUrl, aclContentNew, 'text/turtle')
+        console.log("result",result)
+        //this.acl.status = "OK"
+        await this.checkAcl("after update")
+      }catch(e){
+        this.acl.status = "ERROR : "+e
+        alert(e)
+
+      }
+
+
+
+      // @prefix : <#>.
+      // @prefix acl: <http://www.w3.org/ns/auth/acl#>.
+      // @prefix foaf: <http://xmlns.com/foaf/0.1/>.
+      // @prefix target: <./>.
+      //
+      // :ReadAppend
+      //     a acl:Authorization;
+      //     acl:accessTo target:;
+      //     acl:agentClass acl:AuthenticatedAgent;
+      //     acl:mode acl:Read, acl:Append.
+      //
+      // :ReadWriteControl
+      //     a acl:Authorization;
+      //     acl:accessTo target:;
+      //     acl:default target:;
+      //     acl:agent </profile/card#me>;
+      //     acl:agent <https://bourgeoa.solidcommunity.net/profile/card#me>;
+      //     acl:mode acl:Read, acl:Write, acl:Control.
+
+      //       @prefix : <#>.
+      // @prefix acl: <http://www.w3.org/ns/auth/acl#>.
+      // @prefix foaf: <http://xmlns.com/foaf/0.1/>.
+      // @prefix target: <./>.
+      //
+      // :ReadAppend
+      //     a acl:Authorization;
+      //     acl:accessTo target:;
+      //     acl:agentClass acl:AuthenticatedAgent;
+      //     acl:mode acl:Read, acl:Append.
+      //
+      // :ReadWriteControl
+      //     a acl:Authorization;
+      //     acl:accessTo target:;
+      //     acl:agent </profile/card#me>;
+      //     acl:mode acl:Read, acl:Write, acl:Control.
+
+
+      // if (this.webId != null){
+      //   let inboxFolder = await ldflex.data.from(this.webId)[this.webId]['http://www.w3.org/ns/ldp#inbox']
+      //   let inboxAnnonce = `${inboxFolder}`+"annonces/"
+      //   if( !(await fc.itemExists(inboxAnnonce)) ) {
+      //     await fc.createFolder(inboxAnnonce) // only create if it doesn't already exist
+      //   }
+      //
+      //   let aclObject = await fc.aclUrlParser(inboxAnnonce)
+      //   console.log("inherit acl",aclObject)
+      //   let aclContent = await fc.acl.createContent(inboxAnnonce, aclObject/*, options*/)
+      //   console.log(aclContent)
+      //
+      //   // add an other rule
+      //   let   aclUsers = await fc.acl.addUserMode({}, [{ agent: this.webId }], ['Read', 'Write', 'Control'], ['accessTo', 'default'])
+      //   aclUsers = await fc.acl.addUserMode(aclUsers, [{ agentClass: 'AuthenticatedAgent' }], ['Append'], ['default'])
+      //   aclUsers = await fc.acl.addUserMode(aclUsers, [{ agentClass: 'Agent' }], ['Append', 'Read'], ['accessTo'])
+      //
+      //   //      aclUsers = await fc.acl.addUserMode(aclUsers, [{ agent: 'https://bourgeoa.solidcommunity.net/profile/card#me' }], ['Read', 'Write', 'Control'], ['accessTo', 'default'])
+      //
+      //
+      //   //  let aclUsers = await fc.acl.addUserMode({}, [{ agentClass: 'AuthenticatedAgent'}], ['Read', 'Write'])
+      //   const aclContentNew = await fc.acl.createContent(inboxAnnonce, aclUsers)
+      //   console.log('build an aclContent ' + aclContentNew)
+      //
+      //   const { acl: aclUrl } = await fc.getItemLinks(inboxAnnonce, { links: 'include_possible'})
+      //   //    const links = await fc.getItemLinks(inboxAnnonce, { links: 'include_possible'})
+      //
+      //   //  console.log(links)
+      //   let result = await fc.putFile(aclUrl, aclContentNew, 'text/turtle')
+      //   console.log("result",result)
+
+    },
+  },
+  watch:{
+    webId(){
+      this.init()
+    },
+    path(){
+      this.init()
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
